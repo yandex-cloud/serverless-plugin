92647e96a0e45bd17d0de2f3a166c3b0b567493c
Ivan Zuev <i-zuev@yandex-team.ru>
2021-12-29 15:17:04 +0300

    refactor: rewrite index module to ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,23 @@
-// TBD
-console.log('Nothing here yet');
+import Serverless from 'serverless';
+
+import YandexCloudProvider from './lib/provider/provider';
+import YandexCloudDeploy from './lib/deploy/deploy';
+import YandexCloudRemove from './lib/remove/remove';
+import YandexCloudInvoke from './lib/invoke/invoke';
+import YandexCloudInfo from './lib/info/info';
+
+export default class YandexCloudServerlessPlugin {
+    private readonly serverless: Serverless;
+    private readonly options: Serverless.Options;
+
+    constructor(serverless: Serverless, options: Serverless.Options) {
+        this.serverless = serverless;
+        this.options = options;
+
+        this.serverless.pluginManager.addPlugin(YandexCloudProvider);
+        this.serverless.pluginManager.addPlugin(YandexCloudDeploy);
+        this.serverless.pluginManager.addPlugin(YandexCloudRemove);
+        this.serverless.pluginManager.addPlugin(YandexCloudInvoke);
+        this.serverless.pluginManager.addPlugin(YandexCloudInfo);
+    }
+}
