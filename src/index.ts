bd0cae29ebea271043646648e8f1133b32871ce0
Ivan Zuev <i-zuev@yandex-team.ru>
2022-01-13 18:58:33 +0300

    fix: describe provider

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,88 @@ class YandexCloudServerlessPlugin {
         this.serverless.pluginManager.addPlugin(YandexCloudInvoke);
         this.serverless.pluginManager.addPlugin(YandexCloudInfo);
         this.serverless.pluginManager.addPlugin(YandexCloudLogs);
+
+        this.serverless.configSchemaHandler.defineProvider(YandexCloudProvider.getProviderName(), {
+            definitions: {
+                cloudFunctionRegion: {
+                    enum: [
+                        'ru-central1',
+                    ],
+                },
+                cloudFunctionRuntime: {
+                    // Source: https://cloud.google.com/functions/docs/concepts/exec#runtimes
+                    enum: [
+                        'nodejs10',
+                        'nodejs12',
+                        'nodejs14',
+                        'nodejs16',
+                        'python27',
+                        'python37',
+                        'python38',
+                        'python39',
+                        'go114',
+                        'go116',
+                        'go117',
+                        'java11',
+                        'dotnet31',
+                        'bash',
+                        'php74',
+                        'php8',
+                        'r4.0.2',
+                    ],
+                },
+                cloudFunctionMemory: {
+                    type: 'number',
+                },
+                cloudFunctionEnvironmentVariables: {
+                    type: 'object',
+                    patternProperties: {
+                        '^.*$': { type: 'string' },
+                    },
+                    additionalProperties: false,
+                },
+                resourceManagerLabels: {
+                    type: 'object',
+                    propertyNames: {
+                        type: 'string',
+                        minLength: 1,
+                        maxLength: 63,
+                    },
+                    patternProperties: {
+                        '^[a-z][a-z0-9_.]*$': { type: 'string' },
+                    },
+                    additionalProperties: false,
+                },
+            },
+
+            provider: {
+                properties: {
+                    credentials: { type: 'string' },
+                    project: { type: 'string' },
+                    region: { $ref: '#/definitions/cloudFunctionRegion' },
+                    runtime: { $ref: '#/definitions/cloudFunctionRuntime' }, // Can be overridden by function configuration
+                    serviceAccountEmail: { type: 'string' }, // Can be overridden by function configuration
+                    memorySize: { $ref: '#/definitions/cloudFunctionMemory' }, // Can be overridden by function configuration
+                    timeout: { type: 'string' }, // Can be overridden by function configuration
+                    environment: { $ref: '#/definitions/cloudFunctionEnvironmentVariables' }, // Can be overridden by function configuration
+                    vpc: { type: 'string' }, // Can be overridden by function configuration
+                    labels: { $ref: '#/definitions/resourceManagerLabels' }, // Can be overridden by function configuration
+                },
+            },
+            function: {
+                properties: {
+                    handler: { type: 'string' },
+                    runtime: { $ref: '#/definitions/cloudFunctionRuntime' }, // Override provider configuration
+                    serviceAccountEmail: { type: 'string' }, // Override provider configuration
+                    memorySize: { $ref: '#/definitions/cloudFunctionMemory' }, // Override provider configuration
+                    timeout: { type: 'string' }, // Override provider configuration
+                    environment: { $ref: '#/definitions/cloudFunctionEnvironmentVariables' }, // Override provider configuration
+                    vpc: { type: 'string' }, // Override provider configuration
+                    labels: { $ref: '#/definitions/resourceManagerLabels' }, // Override provider configuration
+                    account: { type: 'string' },
+                },
+            },
+        });
     }
 }
 
